1d966cfacea964eb0f5c67c65aa24184bb4d527d
Manprit Heer <manpritheer@campus-034-007.ucdavis.edu>
2019-04-16 19:47:42 -0700

    Initial Commit

---
+++ studylink_19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  studylink_19
+//
+//  Created by Manprit Heer on 4/16/19.
+//  Copyright © 2019 Manprit Heer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
