c2dbb689521c736256c36582d4383517436b0116
mampz <mkheer@ucdavis.edu>
2019-07-02 14:18:02 -0700

    Welcome Page Inserted

---
+++ studylink-2019/SFiles/QViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QViewController.swift
+//  studylink-2019
+//
+//  Created by Manprit Heer on 7/2/19.
+//  Copyright © 2019 Manprit Heer. All rights reserved.
+//
+
+import UIKit
+
+class QViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ studylink-2019/SFiles/UIViewExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExtensions.swift
+//  studylink-2019
+//
+//  Created by Manprit Heer on 7/2/19.
+//  Copyright © 2019 Manprit Heer. All rights reserved.
+//
+
+import Foundation
