6277c8f37fbf7c5f0ab54902cfe3170ed9dd413a
kchennap <krishna.ceee@gmail.com>
2019-07-09 17:37:45 -0700

    Finished login system. But some bugs in initial sign in with Google

---
+++ studylink-2019/SFiles/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  studylink-2019
+//
+//  Created by Krishna Chenna on 7/3/19.
+//  Copyright © 2019 Manprit Heer. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
