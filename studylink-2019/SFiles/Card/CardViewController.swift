bd7513a6578923eeeabb45b5598fbb4b7cb3450f
mampz <mkheer@ucdavis.edu>
2019-08-09 15:51:32 -0700

    Fixed Merge Conflicts on Card

--- studylink-2019/SFiles/Card/CardViewController.swift
+++ studylink-2019/SFiles/Card/CardViewController.swift
@@ -35,30 +35,38 @@ class CardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        card.layer.cornerRadius = 5;
-        card.layer.masksToBounds = true;
+        image.layer.cornerRadius = 5;
+        image.layer.masksToBounds = true;
         
-        setUpNavbar()
-        
-        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.regular)
-        let blurEffectView = UIVisualEffectView(effect: blurEffect)
-        blurEffectView.frame = view.bounds
-        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        blurEffectView.alpha = 0.9;
-        self.view.addSubview(blurEffectView)
+
         self.view.bringSubviewToFront(self.profilebtn)
-          self.view.bringSubviewToFront(self.chatbtn)
+        self.view.bringSubviewToFront(self.chatbtn)
         self.view.bringSubviewToFront(self.card)
         self.view.bringSubviewToFront(self.image)
-        
         self.view.bringSubviewToFront(self.firstName)
-        
         self.view.bringSubviewToFront(self.lastInitial)
 //        self.view.bringSubviewToFront(self.buttonsView)
         
+        
+   
+        setUpNavbar()
+        setupMenuOptions()
+        setupMenuBar()
+
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = menuBar.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        blurEffectView.alpha = 0.9;
+        self.view.addSubview(blurEffectView)
+        self.tabBarController?.tabBar.isHidden = true
     }
     
+
+    
+    
     private func setUpNavbar(){
+         self.navigationController?.setNavigationBarHidden(true, animated: false)
         let chatButton = UIButton(type: .system)
         let mainLbl = UILabel(frame: CGRect(x: 0, y: 0, width: 34, height: 34))
         
@@ -66,17 +74,17 @@ class CardViewController: UIViewController {
         mainLbl.text = "Find Partners"
         
         navigationItem.titleView = mainLbl
-        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .done, target: nil, action: nil)
+       // self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .done, target: nil, action: nil)
         let chatIcon = UIImage(named: "chat")
         chatButton.setImage(chatIcon, for: .normal)
         chatButton.addTarget(self, action: #selector(chatPage), for: .touchUpInside)
-//        setupMenuOptions()
-//        setupMenuBar()
+
         
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: chatButton)
         
     }
+
 
 //    func setupFilterLabels(){
 //        self.view.addSubview(self.menuBar.menuOps.fltrThreeLbl)
@@ -111,120 +119,158 @@ class CardViewController: UIViewController {
     }
     
     
-    @IBAction func calendarChosen(_ sender: Any) {
-        self.calendarLine.alpha = 1;
-        self.profileLine.alpha = 0;
-        self.mutualLine.alpha = 0;
-    }
+    //
+    //  CardViewController.swift
+    //  studylink-2019
+    //
+    //  Created by Manprit Heer on 7/4/19.
+    //  Copyright © 2019 Manprit Heer. All rights reserved.
+    //
     
-    @IBAction func mutualChosen(_ sender: Any) {
-        self.calendarLine.alpha = 0;
-        self.profileLine.alpha = 0;
-        self.mutualLine.alpha = 1;
-    }
-    
-    @IBAction func profileChosen(_ sender: Any) {
-        self.calendarLine.alpha = 0;
-        self.profileLine.alpha = 1;
-        self.mutualLine.alpha = 0;
-    }
-    
-    
-    @IBAction func toggleMenu(_ sender: Any) {
-        if darkFillView.transform == CGAffineTransform.identity {
-            UIView.animate(withDuration: 1, animations: {
-                self.darkFillView.transform = CGAffineTransform(scaleX: 18, y: 11)
-                self.menuView.transform = CGAffineTransform(translationX:0, y:-67)
-                self.toggleMenuButton.transform = CGAffineTransform(rotationAngle: self.radians(degrees: 180))
-            }) {(true) in
-                
-            }
-        } else{
-            UIView.animate(withDuration: 1, animations: {
-                self.darkFillView.transform = .identity
-                self.menuView.transform = .identity
-                self.toggleMenuButton.transform = .identity
-                
-            }) {(true) in
-                
-            }
-        }
-    }
-    
-    func radians(degrees: Double) -> CGFloat{
-        return CGFloat(degrees * .pi / degrees)
-    }
-    
-    
-    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
-        let card = sender.view!
-        let point = sender.translation(in: view)
-        let yFromCenter = card.center.y - view.center.y
         
-        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
         
-        if yFromCenter < 0 {
-            linkedView.image = UIImage(named: "linked.png")
+        let cellId = "cellId"
+        
+        
+        func addConstraintsWithFormat(_ format: String, views: UIView...) {
+            var viewDict = [String: UIView]()
             
-            linkedView.tintColor = .green
+            for (index, view) in views.enumerated() {
+                let key = "v\(index)"
+                view.translatesAutoresizingMaskIntoConstraints = false
+                viewDict[key] = view
+            }
             
-        } else {
-            linkedView.image = nil;
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: viewDict))
         }
         
-        linkedView.alpha = abs(yFromCenter) / view.center.y
+        let menuBar: MenuBar = {
+            let mb = MenuBar()
+            return mb
+        }()
         
-        if sender.state == UIGestureRecognizer.State.ended {
-            if card.center.y < 75 {
-                //Move the card all the way up, off the screen
-                UIView.animate(withDuration: 0.3, animations: {
-                    card.center = CGPoint(x: card.center.x , y: card.center.y - 200 )
-                    card.alpha = 0
-                })
-                performSegue(withIdentifier: "swipeUp", sender: self)
-                return
-            } else if card.center.x < 75 {
-                //Move to left side to previous card
-                UIView.animate(withDuration: 0.3, animations: {
-                    card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
-                    card.alpha = 0;
-                })
-                return
-                
-                //FIX ME: INSERT CODE FOR PREVIOUS CARD HERE
-                
-            } else if card.center.x > (view.frame.width - 85){
-                //Move to right side to next card
-                UIView.animate(withDuration: 0.3, animations: {
-                    card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
-                    card.alpha = 0;
-                })
-                
-                //FIX ME: INSERT CODE FOR NEW CARD HERE
-                
-                return
+        let menuOptions: MenuOptions = {
+            let mo = MenuOptions()
+            return mo
+        }()
+        
+        private func setupMenuBar(){
+            view.addSubview(menuBar)
+            
+            addConstraintsWithFormat("H:|[v0]|", views: menuBar)
+            addConstraintsWithFormat("V:|[v0(55)]", views: menuBar)
+            menuBar.transform = CGAffineTransform(translationX: 0, y: 615)
+            
+            
+        }
+        
+        private func setupMenuOptions(){
+            view.addSubview(menuBar.menuOps)
+            addConstraintsWithFormat("H:|[v0]|", views: menuBar.menuOps)
+            addConstraintsWithFormat("V:|[v0(215)]", views: menuBar.menuOps)
+            
+            menuBar.menuOps.transform = CGAffineTransform(translationX: 0, y: 395)
+            
+        }
+        
+        
+        @IBAction func toggleMenu(_ sender: Any) {
+            if darkFillView.transform == CGAffineTransform.identity {
+                UIView.animate(withDuration: 1, animations: {
+                    self.darkFillView.transform = CGAffineTransform(scaleX: 18, y: 11)
+                    self.menuView.transform = CGAffineTransform(translationX:0, y:-67)
+                    self.toggleMenuButton.transform = CGAffineTransform(rotationAngle: self.radians(degrees: 180))
+                }) {(true) in
+                    
+                }
+            } else{
+                UIView.animate(withDuration: 1, animations: {
+                    self.darkFillView.transform = .identity
+                    self.menuView.transform = .identity
+                    self.toggleMenuButton.transform = .identity
+                    
+                }) {(true) in
+                    
+                }
             }
-            //go back to center
+        }
+        
+        func radians(degrees: Double) -> CGFloat{
+            return CGFloat(degrees * .pi / degrees)
+        }
+        
+        
+        @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+            let card = sender.view!
+            let point = sender.translation(in: view)
+            let yFromCenter = card.center.y - view.center.y
+            
+            card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+            
+            if yFromCenter < 0 {
+                linkedView.image = UIImage(named: "linked.png")
+                
+                linkedView.tintColor = .green
+                
+            } else {
+                linkedView.image = nil;
+            }
+            
+            linkedView.alpha = abs(yFromCenter) / view.center.y
+            
+            if sender.state == UIGestureRecognizer.State.ended {
+                if card.center.y < 75 {
+                    //Move the card all the way up, off the screen
+                    UIView.animate(withDuration: 0.3, animations: {
+                        card.center = CGPoint(x: card.center.x , y: card.center.y - 200 )
+                        card.alpha = 0
+                    })
+                    performSegue(withIdentifier: "swipeUp", sender: self)
+                    return
+                } else if card.center.x < 75 {
+                    //Move to left side to previous card
+                    UIView.animate(withDuration: 0.3, animations: {
+                        card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
+                        card.alpha = 0;
+                    })
+                    return
+                    
+                    //FIX ME: INSERT CODE FOR PREVIOUS CARD HERE
+                    
+                } else if card.center.x > (view.frame.width - 85){
+                    //Move to right side to next card
+                    UIView.animate(withDuration: 0.3, animations: {
+                        card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
+                        card.alpha = 0;
+                    })
+                    
+                    //FIX ME: INSERT CODE FOR NEW CARD HERE
+                    
+                    return
+                }
+                //go back to center
+                resetCard()
+            }
+            
+        }
+        func resetCard(){
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                self.card.center = self.view.center
+                self.linkedView.alpha = 0
+                self.card.alpha = 1
+            })
+            
+            
+        }
+        
+        @IBAction func resetBtn(_ sender: Any) {
             resetCard()
         }
         
-    }
-    func resetCard(){
-        
-        UIView.animate(withDuration: 0.2, animations: {
-            self.card.center = self.view.center
-            self.linkedView.alpha = 0
-            self.card.alpha = 1
-        })
-        
-        
-    }
-    
-    @IBAction func resetBtn(_ sender: Any) {
-        resetCard()
-    }
-    
 }
+
+
 
 
 
